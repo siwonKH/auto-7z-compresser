a21746621a125263ebf071da17caa0275391b5c4
siwonKH <kanghyunkim313@gmail.com>
2021-06-25 21:32:26 +0900

    added remark

--- main.py
+++ main.py
@@ -4,41 +4,48 @@ import multiprocessing
 
 
 def parse(name, is_list: bool):
-    config = "".join(open("config.txt", 'r', encoding='utf=8').readlines())
-    parsed_list = config.replace('\n', "").split(name)[1].split('}')[0].split('{')[1].split('"')
+    # reads texts
+    config = "".join(open("config.txt", 'r', encoding='utf=8').readlines()).replace('\n', "")
+    # parses object named {name}
+    parsed_list = config.split(name)[1].split('}')[0].split('{')[1].split('"')
+    # returns list or string, depends on parsing option
     if not is_list:
         return parsed_list[0].strip()
+
     for i in range(0, len(parsed_list), 2):
         del parsed_list[i-int(i/2)]
     return parsed_list
 
 
 def prepare(folder):
+    # parse configurations
     files = parse("files", is_list=True)
     location = parse("location", is_list=True)
     exclude = parse("exclude_folder", is_list=True)
     cores = parse("cores", is_list=False)
+    # runs compress progress
     compress(folder, files, location, exclude, cores)
 
 
 def compress(folder: str, files, location, exclude, cores):
     date = datetime.datetime.now().strftime('%y-%m-%d')
     name = folder.split('\\')[-1]
-    if location[files.index(folder)] == "" and files.index(folder) != 0:
-        i = files.index(folder)
-        while i != -1:
-            if location[i] == "":
-                i -= 1
-                continue
+    # if location is blank, lookup latest location
+    i = files.index(folder)
+    while i != -1:
+        if location[i] != "":
             location = location[i]
             break
-    else:
-        location = location[files.index(folder)]
-    if not location.endswith("\\") and location != "":
-        location += "\\"
+        i -= 1
+    # beautify form
+    location = location.strip("\\")
+    location += "\\"
+    folder = folder.strip("\\")
+    # make exclude_option to string
     exclude_str = ""
     for string in exclude:
         exclude_str += f"-xr!{string} "
+    # 7z command line
     os.system(f'7z a -t7z "{location}{date}\\{name}-{date}" "{folder}" -mmt{cores} -mx=1 {exclude_str}')
 
 
